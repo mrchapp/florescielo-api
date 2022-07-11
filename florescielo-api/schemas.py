010ff31d0ef975cf93461b81e06ed3405a58233a
Daniel Díaz <yosoy@danieldiaz.org>
2022-07-11 11:40:13 -0500

    schemas: Sort schemas alphabetically
    Signed-off-by: Daniel Díaz <yosoy@danieldiaz.org>

--- florescielo-api/schemas.py
+++ florescielo-api/schemas.py
@@ -1,12 +1,12 @@
 from pydantic import BaseModel
 
 
-class FloresCieloTimesRequest(BaseModel):
-    DeviceID: str
-    TS: int
-
-
 class FloresCieloDeviceInfo(BaseModel):
     DeviceID: str
     InfoVersion: int
     DeviceInfo: str
+
+
+class FloresCieloTimesRequest(BaseModel):
+    DeviceID: str
+    TS: int
